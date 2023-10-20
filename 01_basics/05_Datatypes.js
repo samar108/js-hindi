4d8b16a49fd99ee86c9d95ee5d98fe84919c8205
samar108 <96164298+samar108@users.noreply.github.com>
2023-10-20 22:45:00 +0000

    stack and head

--- 01_basics/05_Datatypes.js
+++ 01_basics/05_Datatypes.js
@@ -4,3 +4,13 @@ const myFunction = function(){
 
 
 console.log(typeof myFunction);
+
+let user = {
+    name: "samar",
+    id: 3443,
+}
+
+let user2 = user;
+console.log(user.name);
+console.log(user.id);         // primitive goes in stack 
+                          // goes in heap
