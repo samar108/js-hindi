27cd2f138cbfa43ec5aad08b89d6b1a508efc5ef
samar108 <96164298+samar108@users.noreply.github.com>
2023-10-20 20:41:03 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,20 @@
+let score = 30;
+
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+
+console.log(typeof(score));
+
+let x = 1;
+
+let y = Boolean(x)
+
+console.log(y)
+
+let number = 94
+let str = String(number)
+
+console.log(str)
+console.log(typeof(str));
