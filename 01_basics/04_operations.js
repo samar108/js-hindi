58ad0d749e896a8cde60d69fb11fe62904b327b0
samar108 <96164298+samar108@users.noreply.github.com>
2023-10-20 20:59:12 +0000

    operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,18 @@
+let  number = 34
+
+let negNumber = -number
+
+// console.log(negNumber)
+
+
+// console.log(2**3) // two ki power 3 ho jiti equals to 8
+
+// console.log("1" + 2 + 2)  // all three were treated as strings 
+
+// console.log(1 + 2 + "2")  // 1 and 2 add ho gye
+
+let gameCounter = 100
+// gameCounter++
+// ++gameCounter
+console.log(gameCounter);
+
