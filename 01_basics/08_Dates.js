cc83dc48c72ea06b9c0c206b1727026d12bd5d0b
samar108 <96164298+samar108@users.noreply.github.com>
2023-10-21 14:24:10 +0000

    date and time

---
+++ 01_basics/08_Dates.js
@@ -0,0 +1,18 @@
+// dates
+
+let myDate = new Date()
+console.log(myDate)
+console.log(myDate.toString());
+
+let myCreateDate = new Date(2023 , 0 , 23)
+
+console.log(myCreateDate.getFullYear());
+// let myCreateDate = new Date("01-04-2033")
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp)
+
+
+// console.log(myCreateDate.toString());
+
+// console.log(myCreateDate.getTime());
