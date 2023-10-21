f38a7c2ef67d2663419d8aa798809fd1b36b02cb
samar108 <96164298+samar108@users.noreply.github.com>
2023-10-21 14:04:11 +0000

    math and num

---
+++ 01_basics/07_number.js
@@ -0,0 +1,30 @@
+const value = 434;
+
+console.log(value);
+
+const num = new Number(100);
+console.log(num);
+
+console.log(num.toString().length);
+
+console.log(num.toFixed(3));
+
+const otherNumber = 34.4354
+
+console.log(otherNumber.toPrecision(5));
+
+const hundreds = 100000000
+console.log(hundreds.toLocaleString());
+console.log(hundreds.toLocaleString("en-IN"));
+
+console.log(Number.MAX_VALUE);
+
+console.log(Math);
+console.log(Math.abs(-343));
+console.log(Math.round(4.34));
+console.log(Math.ceil(43.43));
+console.log(Math.floor(43.43));
+console.log(Math.sqrt(4));
+
+console.log(Math.floor(Math.random() *10)+1);
+
