26c4dfd54c81d39e3a01262d652fe39d768e0b08
samar108 <96164298+samar108@users.noreply.github.com>
2023-10-21 15:12:13 +0000

    arrays and methods

---
+++ 01_basics/09_array.js
@@ -0,0 +1,20 @@
+let array = [32 , 34 , 53 , 643 , 64 , "samar"]
+
+
+console.log(array[5]);
+
+const arr = ["saktiman" , "superman" , "batman"];
+
+const myArr = new Array(2, 42, 42, 564 ,64 )
+
+console.log(myArr)
+console.log(myArr[4])
+console.log(myArr.length)
+
+console.log(myArr.push(42));
+console.log(myArr);
+console.log(myArr[5]);
+console.log(myArr.pop(42));
+console.log(myArr[5]);
+console.log(myArr);
+console.log(myArr.shift()); 
