3c38b5c6cf3f4e85d5823bbfe1e48a2fefff81d3
samar108 <96164298+samar108@users.noreply.github.com>
2023-10-22 12:30:43 +0000

    iife

---
+++ 01_basics/16_IIFE.js
@@ -0,0 +1,8 @@
+(function chai(){
+    console.log(`Db connected`);
+})();                  // semi-colon is required
+
+// chai()
+(function aurcode(name){
+    console.log(`Db connected done ${name} hello`);   // named IIFE 
+})("samarjeet ");
